abe4c98b267d3d9efe47345434c30992c76c491b
AyahDweikat <dweikatayah99@gmail.com>
2023-10-04 21:32:58 +0300

    initialize app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Main from './Pages/Main/Main.tsx'
+import Signup from './Pages/SignUp/Signup.tsx'
+
+function App() {
+
+  return (
+    <>
+      <Signup />
+      <Main />
+    </>
+  )
+}
+
+export default App
